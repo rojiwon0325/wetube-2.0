25150e4ccb510aa663e7f10a05615e4df8a5567e
rojiwon0325 <68629004+rojiwon0325@users.noreply.github.com>
2021-06-25 17:26:07 +0900

    ~#6.4

--- src/routers/controllers/globalController.js
+++ src/routers/controllers/globalController.js
@@ -1,20 +1,43 @@
-
+let user = {
+    username: "jiwon"
+}
+user = null;
+const videos = [
+    {
+        title: "video title",
+        rating: 2,
+        comments: 3,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "video title2",
+        rating: 3,
+        comments: 4,
+        createdAt: "3 minutes ago",
+        views: 9,
+        id: 2
+    }
+];
 
 export const home = (req, res) => {
-    res.render("home");
+    res.render("home", { user, videos });
 }
 
 export const login = (req, res) => {
-    res.render("login");
+    res.render("login", { pageTitle: "Login |" });
 }
 
 
 export const results = (req, res) => {
-    res.render("search");
+    console.log(req.query);
+    res.render("search", { pageTitle: "results |" });
 }
 
 
 export const watch = (req, res) => {
-    console.log(req.query);
-    res.render("watch");
+    const { v } = req.query;
+    const video = videos.find(elem => elem.id === parseInt(v, 10));
+    res.render("watch", { pageTitle: video ? `${video.title} |` : "", video });
 }
---
+++ src/routers/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const getUpload = (req, res) => {
+    res.render("studio", { title: `${res.locals.title} Studio` });
+};
+export const postUpload = (req, res) => {
+};
+export const getEditVideo = (req, res) => {
+};
+export const postEditVideo = (req, res) => {
+};
